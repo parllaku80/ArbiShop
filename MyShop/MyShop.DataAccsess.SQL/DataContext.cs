2f8ed5c32a9d867245e9f9b40554837cf6384fd7
Arbi Parllaku <parllaku80@gmail.com>
2019-08-17 19:09:02 +0200

    A little introduction to Entity Framework

---
+++ MyShop/MyShop.DataAccsess.SQL/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccsess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection") // looks in web.config for the inserted string
+                {
+
+            }
+    }
+    
+}
